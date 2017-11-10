4d4dc9e208bfe0882d7b32da086ec3ad9b7e9f17
vedabrat <vedabrat>
2017-11-10 16:10:04 -0500

    chkpt20 done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,17 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-
   end
-  def create
 
-  @post = Post.new
+  def create
+    @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
 
-# #10
     if @post.save
-# #11
       flash[:notice] = "Post was saved."
       redirect_to @post
     else
-# #12
       flash.now[:alert] = "There was an error saving the post. Please try again."
       render :new
     end
@@ -23,14 +19,39 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-
   end
 
   def new
     @post = Post.new
-
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "There was an error deleting the post."
+      render :show
+    end
   end
 end
